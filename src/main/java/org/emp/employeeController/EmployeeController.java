2b982a48404303c92d651e6bd7a228c649c8320d
sharada-marasinha <sharadamarasinha@gmail.com>
2024-06-13 21:22:22 +0530

    get employee list

--- src/main/java/org/emp/employeeController/EmployeeController.java
+++ src/main/java/org/emp/employeeController/EmployeeController.java
@@ -1,10 +1,7 @@
 package org.emp.employeeController;
 
 import org.emp.dto.Employee;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +14,10 @@ public class EmployeeController {
     @PostMapping("add-employee")
     public void addEmployee(@RequestBody Employee employee) {
         employeeList.add(employee);
+    }
+    @GetMapping("get-all")
+    public List<Employee> getAll(){
+        return employeeList;
     }
 }
 
