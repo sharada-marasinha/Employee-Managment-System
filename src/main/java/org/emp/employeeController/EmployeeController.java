a8c05a224b3f1c49f13ecec9ee9016c0ff3ed249
sharada-marasinha <sharadamarasinha@gmail.com>
2024-06-13 22:12:06 +0530

    hibernate and add | get All rest apis

--- src/main/java/org/emp/employeeController/EmployeeController.java
+++ src/main/java/org/emp/employeeController/EmployeeController.java
@@ -1,6 +1,11 @@
 package org.emp.employeeController;
 
+import lombok.RequiredArgsConstructor;
 import org.emp.dto.Employee;
+import org.emp.entity.EmployeeEntity;
+import org.emp.service.EmployeeService;
+import org.emp.service.impl.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,15 +14,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/emp-controller")
 //http://localhost:8080/emp-controller/add-employee
+@RequiredArgsConstructor
 public class EmployeeController {
-    List<Employee> employeeList = new ArrayList();
+    final EmployeeService service;
     @PostMapping("add-employee")
     public void addEmployee(@RequestBody Employee employee) {
-        employeeList.add(employee);
+        service.addEmployee(employee);
     }
+
     @GetMapping("get-all")
-    public List<Employee> getAll(){
-        return employeeList;
+    public List<EmployeeEntity> getAll() {
+      return service.getAll();
     }
 }
 
---
+++ src/main/java/org/emp/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package org.emp.service.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.emp.dto.Employee;
+import org.emp.entity.EmployeeEntity;
+import org.emp.repository.EmployeeRepository;
+import org.emp.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    final EmployeeRepository repository;
+    @Override
+    public void addEmployee(Employee employee) {
+        EmployeeEntity employeeEntity = new ObjectMapper().convertValue(employee, EmployeeEntity.class);
+        repository.save(employeeEntity);
+    }
+
+    @Override
+    public List<EmployeeEntity> getAll() {
+        return repository.findAll();
+    }
+}
